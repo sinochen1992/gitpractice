9912314d3405a4fcc977d384ca656af4400baeb5
Chen Ding <sinochen1992@gmail.com>
2016-09-09 15:43:57 -0400

    addednewline

--- helloworld.py
+++ helloworld.py
@@ -5,6 +5,6 @@ Spyder Editor
 This is a temporary script file.
 """
 
-print("Date:Wednesday Sep 7 2016\
-\nHello World!\
+print("Date:Wednesday Sep 7 2016 \
+\nHello World! \
 \nGeoff is teaching me how to use git")
